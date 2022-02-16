e283282a692ccdd5f6b53e6e3df30ee6406dadb7
Nikolay Elesin <elesinn@ya.ru>
2022-02-16 09:20:56 +0300

    feat: add advanced Table component

--- src/features/tokens/ui/index.tsx
+++ src/features/tokens/ui/index.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
+
 import { useAtom } from 'jotai';
 
 import { useTokensInfo } from 'api/tezPrices';
 import { addressSearchAtom } from 'features/site-layout';
+import Table, { AvatarCell } from 'shared/table';
 
 import { useTokensBalances } from '../../../api/tokens/index';
 
@@ -9,6 +12,57 @@ export const TokensList = () => {
   const [userAddress] = useAtom(addressSearchAtom);
   const { data: tokensBalances } = useTokensBalances({ userAddress });
   const { data: tokensInfo } = useTokensInfo();
+
+  const tableData = React.useMemo(() => {
+    return tokensBalances?.map((token) => ({
+      imgUrl:
+        token.artifact_uri?.replace(
+          'ipfs://',
+          'https://api.dipdup.net/thumbnail/',
+        ) ||
+        token.thumbnail_uri?.replace(
+          'ipfs://',
+          'https://api.dipdup.net/thumbnail/',
+        ) ||
+        `https://services.tzkt.io/v1/avatars/${token.contract.address}` ||
+        undefined,
+      symbol: token.symbol,
+      name: `${token.name || ''} (${token.contract.alias})`,
+      // alias: token.contract.alias,
+      balance: Number(
+        Number(token.balance) / 10 ** Number(token.decimals),
+      ).toFixed(3),
+      balanceinTzx:
+        tokensInfo &&
+        Number(
+          Number(
+            (Number(token.balance) / 10 ** Number(token.decimals)) *
+              (tokensInfo[token.symbol || '']?.currentPrice || 0),
+          ).toFixed(3),
+        ),
+    }));
+  }, [tokensBalances, tokensInfo]);
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Token',
+        accessor: 'symbol',
+        Cell: AvatarCell,
+        imgAccessor: 'imgUrl',
+        descAccessor: 'name',
+      },
+      {
+        Header: 'Quantity',
+        accessor: 'balance',
+      },
+      {
+        Header: 'Estimated Value',
+        accessor: 'balanceinTzx',
+      },
+    ],
+    [],
+  );
 
   if (!tokensBalances || !tokensInfo) {
     return null;
@@ -27,118 +81,11 @@ export const TokensList = () => {
         </div>
       </div>
       <div className="flex flex-col">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
-                    >
-                      Token
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
-                    >
-                      Quantity
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
-                    >
-                      Estimated Value
-                    </th>
-                    {/* <th
-                    scope="col"
-                    className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
-                  >
-                    Role
-                  </th>
-                  <th scope="col" className="relative px-6 py-3">
-                    <span className="sr-only">Edit</span>
-                  </th> */}
-                  </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                  {tokensBalances.map((token) => (
-                    <tr key={token.id}>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="flex items-center">
-                          <div className="flex-shrink-0 w-10 h-10">
-                            <img
-                              className="w-10 h-10 rounded-full"
-                              src={
-                                token.artifact_uri?.replace(
-                                  'ipfs://',
-                                  'https://api.dipdup.net/thumbnail/',
-                                ) ||
-                                token.thumbnail_uri?.replace(
-                                  'ipfs://',
-                                  'https://api.dipdup.net/thumbnail/',
-                                ) ||
-                                `https://services.tzkt.io/v1/avatars/${token.contract.address}` ||
-                                undefined
-                              }
-                              alt=""
-                            />
-                          </div>
-                          <div className="ml-4">
-                            <div className="text-sm font-medium text-gray-900">
-                              {token.symbol}
-                            </div>
-                            <div className="text-sm text-gray-500">
-                              {token.name} ({token.contract.alias})
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">
-                          {/* {person.title} */}
-                          {Number(
-                            Number(token.balance) /
-                              10 ** Number(token.decimals),
-                          ).toFixed(3)}
-                        </div>
-                        <div className="text-sm text-gray-500">
-                          {/* {person.department} */}
-                          {token.symbol}
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">
-                          {Number(
-                            Number(
-                              (Number(token.balance) /
-                                10 ** Number(token.decimals)) *
-                                (tokensInfo[token.symbol || '']?.currentPrice ||
-                                  0),
-                            ).toFixed(3),
-                          )}
-                        </div>
-                        <div className="text-sm text-gray-500">ꜩ</div>
-                      </td>
-                      {/* <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
-                      {person.role}
-                    </td>
-                    <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                      <a
-                        href="#"
-                        className="text-indigo-600 hover:text-indigo-900"
-                      >
-                        Edit
-                      </a>
-                    </td> */}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+        {tableData ? (
+          <Table columns={columns} data={tableData} />
+        ) : (
+          'No tokens yet :('
+        )}
       </div>
     </>
   );
---
+++ src/shared/table/index.tsx
@@ -0,0 +1,381 @@
+/* eslint-disable react/jsx-key */
+import React from 'react';
+
+import {
+  ChevronDoubleLeftIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronDoubleRightIcon,
+} from '@heroicons/react/solid';
+import {
+  useTable,
+  useFilters,
+  useGlobalFilter,
+  useAsyncDebounce,
+  useSortBy,
+  usePagination,
+  Row,
+  Column,
+} from 'react-table';
+
+import { Button, PageButton } from 'shared/ui/button';
+
+export function SortIcon({ className }: any) {
+  return (
+    <svg
+      className={className}
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 320 512"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41zm255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41z"></path>
+    </svg>
+  );
+}
+
+export function SortUpIcon({ className }: any) {
+  return (
+    <svg
+      className={className}
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 320 512"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M279 224H41c-21.4 0-32.1-25.9-17-41L143 64c9.4-9.4 24.6-9.4 33.9 0l119 119c15.2 15.1 4.5 41-16.9 41z"></path>
+    </svg>
+  );
+}
+
+export function SortDownIcon({ className }: any) {
+  return (
+    <svg
+      className={className}
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 320 512"
+      height="1em"
+      width="1em"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41z"></path>
+    </svg>
+  );
+}
+
+function GlobalFilter({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}: {
+  preGlobalFilteredRows: Row<object>[];
+  globalFilter: any;
+  setGlobalFilter: (filterValue: any) => void;
+}) {
+  const count = preGlobalFilteredRows.length;
+  const [value, setValue] = React.useState(globalFilter);
+  const onChange = useAsyncDebounce((value) => {
+    setGlobalFilter(value || undefined);
+  }, 200);
+
+  return (
+    <label className="flex items-baseline gap-x-2">
+      <span className="text-gray-700">Search: </span>
+      <input
+        type="text"
+        className="border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        value={value || ''}
+        onChange={(e) => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+        placeholder={`${count} records...`}
+      />
+    </label>
+  );
+}
+
+// // This is a custom filter UI for selecting
+// // a unique option from a list
+// export function SelectColumnFilter({
+//   column: { filterValue, setFilter, preFilteredRows, id, render },
+// }) {
+//   // Calculate the options for filtering
+//   // using the preFilteredRows
+//   const options = React.useMemo(() => {
+//     const options = new Set();
+//     preFilteredRows.forEach((row) => {
+//       options.add(row.values[id]);
+//     });
+//     return [...options.values()];
+//   }, [id, preFilteredRows]);
+
+//   // Render a multi-select box
+//   return (
+//     <label className="flex items-baseline gap-x-2">
+//       <span className="text-gray-700">{render('Header')}: </span>
+//       <select
+//         className="border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+//         name={id}
+//         id={id}
+//         value={filterValue}
+//         onChange={(e) => {
+//           setFilter(e.target.value || undefined);
+//         }}
+//       >
+//         <option value="">All</option>
+//         {options.map((option, i) => (
+//           <option key={i} value={option}>
+//             {option}
+//           </option>
+//         ))}
+//       </select>
+//     </label>
+//   );
+// }
+
+export function AvatarCell({ value, column, row }: any) {
+  return (
+    <div className="flex items-center">
+      <div className="flex-shrink-0 w-10 h-10">
+        <img
+          className="w-10 h-10 rounded-full"
+          src={row.original[column.imgAccessor]}
+          alt=""
+        />
+      </div>
+      <div className="ml-4">
+        <div className="text-sm font-medium text-gray-900">{value}</div>
+        <div className="text-sm text-gray-500">
+          {row.original[column.descAccessor]}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Table({
+  columns,
+  data,
+}: {
+  columns: readonly Column<object>[];
+  data: readonly object[];
+}) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    pageOptions,
+    state,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
+    usePagination,
+  );
+
+  return (
+    <>
+      <div className="sm:flex sm:gap-x-2">
+        <GlobalFilter
+          preGlobalFilteredRows={preGlobalFilteredRows}
+          globalFilter={state.globalFilter}
+          setGlobalFilter={setGlobalFilter}
+        />
+        {headerGroups.map((headerGroup) =>
+          headerGroup.headers.map((column) =>
+            column.Filter ? (
+              <div className="mt-2 sm:mt-0" key={column.id}>
+                {column.render('Filter')}
+              </div>
+            ) : null,
+          ),
+        )}
+      </div>
+      {/* table */}
+      <div className="flex flex-col mt-4">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+              <table
+                {...getTableProps()}
+                className="min-w-full divide-y divide-gray-200"
+              >
+                <thead className="bg-gray-50">
+                  {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map((column) => (
+                        // Add the sorting props to control sorting. For this example
+                        // we can add them into the header props
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase group"
+                          {...column.getHeaderProps(
+                            column.getSortByToggleProps(),
+                          )}
+                        >
+                          <div className="flex items-center justify-between">
+                            {column.render('Header')}
+                            {/* Add a sort direction indicator */}
+                            <span>
+                              {column.isSorted ? (
+                                column.isSortedDesc ? (
+                                  <SortDownIcon className="w-4 h-4 text-gray-400" />
+                                ) : (
+                                  <SortUpIcon className="w-4 h-4 text-gray-400" />
+                                )
+                              ) : (
+                                <SortIcon className="w-4 h-4 text-gray-400 opacity-0 group-hover:opacity-100" />
+                              )}
+                            </span>
+                          </div>
+                        </th>
+                      ))}
+                    </tr>
+                  ))}
+                </thead>
+                <tbody
+                  {...getTableBodyProps()}
+                  className="bg-white divide-y divide-gray-200"
+                >
+                  {page.map((row) => {
+                    // new
+                    prepareRow(row);
+                    return (
+                      <tr {...row.getRowProps()}>
+                        {row.cells.map((cell) => {
+                          return (
+                            <td
+                              {...cell.getCellProps()}
+                              className="px-6 py-4 whitespace-nowrap"
+                              role="cell"
+                            >
+                              {/* @ts-ignore */}
+                              {cell.column.Cell.name === 'defaultRenderer' ? (
+                                <div className="text-sm text-gray-500">
+                                  {cell.render('Cell')}
+                                </div>
+                              ) : (
+                                cell.render('Cell')
+                              )}
+                            </td>
+                          );
+                        })}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Pagination */}
+      <div className="flex items-center justify-between py-3">
+        <div className="flex justify-between flex-1 sm:hidden">
+          <Button onClick={() => previousPage()} disabled={!canPreviousPage}>
+            Previous
+          </Button>
+          <Button onClick={() => nextPage()} disabled={!canNextPage}>
+            Next
+          </Button>
+        </div>
+        <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+          <div className="flex items-baseline gap-x-2">
+            <span className="text-sm text-gray-700">
+              Page <span className="font-medium">{state.pageIndex + 1}</span> of{' '}
+              <span className="font-medium">{pageOptions.length}</span>
+            </span>
+            <label>
+              <span className="sr-only">Items Per Page</span>
+              <select
+                className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                value={state.pageSize}
+                onChange={(e) => {
+                  setPageSize(Number(e.target.value));
+                }}
+              >
+                {[5, 10, 20].map((pageSize) => (
+                  <option key={pageSize} value={pageSize}>
+                    Show {pageSize}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+          <div>
+            <nav
+              className="relative z-0 inline-flex -space-x-px rounded-md shadow-sm"
+              aria-label="Pagination"
+            >
+              <PageButton
+                className="rounded-l-md"
+                onClick={() => gotoPage(0)}
+                disabled={!canPreviousPage}
+              >
+                <span className="sr-only">First</span>
+                <ChevronDoubleLeftIcon
+                  className="w-5 h-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </PageButton>
+              <PageButton
+                onClick={() => previousPage()}
+                disabled={!canPreviousPage}
+              >
+                <span className="sr-only">Previous</span>
+                <ChevronLeftIcon
+                  className="w-5 h-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </PageButton>
+              <PageButton onClick={() => nextPage()} disabled={!canNextPage}>
+                <span className="sr-only">Next</span>
+                <ChevronRightIcon
+                  className="w-5 h-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </PageButton>
+              <PageButton
+                className="rounded-r-md"
+                onClick={() => gotoPage(pageCount - 1)}
+                disabled={!canNextPage}
+              >
+                <span className="sr-only">Last</span>
+                <ChevronDoubleRightIcon
+                  className="w-5 h-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </PageButton>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Table;
