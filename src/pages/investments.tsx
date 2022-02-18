5bfdf5d16b4ba76ecfdf887edc18aa1b9f5fd2bf
Nikolay Elesin <elesinn@ya.ru>
2022-02-18 13:58:52 +0300

    style(investments): farm description

--- src/pages/investments.tsx
+++ src/pages/investments.tsx
@@ -4,6 +4,10 @@ import { TokensList } from 'features/tokens/ui';
 
 const Index = () => (
   <SiteLayout title="Investments">
+    <h2 className="text-xl font-semibold text-gray-900">Plenty investments</h2>
+    <span className="text-gray-500 text-m">
+      (only active Farms is displayed)
+    </span>
     <InvestmentsList />
     <TokensList />
   </SiteLayout>
