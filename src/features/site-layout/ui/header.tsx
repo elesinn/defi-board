1fd3ff747173246dc98e865e662cf87a1aa1bbe9
Kirill Koshelev <olegartor7@gmail.com>
2022-02-23 13:49:11 +0300

    style(header): search icon padding

--- src/features/site-layout/ui/header.tsx
+++ src/features/site-layout/ui/header.tsx
@@ -59,13 +59,13 @@ export function Header({
             <label htmlFor="search-field" className="sr-only">
               Search
             </label>
-            <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-              <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
+            <div className="relative w-full text-gray-600 focus-within:text-main-500">
+              <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
                 <SearchIcon className="w-5 h-5" aria-hidden="true" />
               </div>
               <input
                 id="search-field"
-                className="block w-full h-full py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 border-transparent focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm bg-main-200 rounded-lg"
+                className="block w-full h-full py-2 pl-9 pr-3 text-gray-900 placeholder-gray-500 border-transparent focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm bg-main-200 rounded-lg"
                 placeholder="Tezos Address"
                 type="search"
                 value={address}
